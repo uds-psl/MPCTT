3017a70f4eb96e0feeb3b40fc33b9b1d488d66ac
smolka <smolka@ps.uni-saarland.de>
2021-04-20 20:19:16 +0200

    blah

--- coq/pat.v
+++ coq/pat.v
@@ -1,51 +1,61 @@
-(** We use Coq's standard definitions for falsity, truth, 
-    conjunctions, and disjunctions. *)              
+(** We use Coq's definitions for falsity, truth, 
+    conjunctions, and disjunctions. *)
+
+Module Show_Definitions.
+  Inductive False := .
+  Inductive True := I.
+  (** We cheat with negation.  Will be explained later. *)
+  Notation "~ P" := (P -> False).
+  Inductive and (P Q: Prop) : Prop :=
+  | conj: P -> Q -> and P Q.
+  Inductive or (P Q: Prop) : Prop :=
+  | or_introl: P -> or P Q
+  | or_intror: Q -> or P Q.
+End Show_Definitions.
 
 Print False.
 Print True.
+Print and.
+Print or.
 
 Definition elim_False
   : forall Z: Type, False -> Z
   := fun Z a => match a with end.
 
-Definition elim_True
-  : forall p: True -> Type, p I -> forall a, p a
-  := fun p b a => match a with I => b end.
-
 Locate "/\".
 Check and.
 Print and.
-About and.
 Print or.
+About or.  (* Overloaded implicit arguments *)
 
 Definition match_and
   : forall X Y Z: Prop, X /\ Y -> (X -> Y -> Z) -> Z
-  := fun X Y Z a f => match a with conj x y => f x y end.
+  := fun X Y Z a e => match a with conj x y => e x y end.
 
 Definition match_or
   : forall X Y Z: Prop, X \/ Y -> (X -> Z) -> (Y -> Z) -> Z
-  := fun X Y Z a f g => match a with or_introl x => f x | or_intror y => g y end.
+  := fun X Y Z a e1 e2 => match a with or_introl x => e1 x | or_intror y => e2 y end.
 
 Section Demo.
-  Variables X Y Z : Prop.
+  Variables X Y Z: Prop.
 
-  (** We type check canonical proofs.  Note how 
+  (** We type check normal proofs.  Note how 
       implicit arguments are supplied if necessary.
       Also note how parentheses are ommitted in the types derived. **)      
 
   Check fun x:X => x.
-  Check fun (x:X) (y:Y) => x.
-  Check fun (x:X) (y:Y) => y.
-  Check fun (f:X->Y->Z) y x => f x y.
-  Check @conj X Y.
+  Check fun (x: X) (y: Y) => x.
+  Check fun (x: X) (y: Y) => y.
+  Check fun (f: X -> Y -> Z) y x => f x y.
   Check fun h: X /\ Y => match h with conj x y => x end.
   Check fun h: X /\ Y => match h with conj x y => y end.
   Check fun h: X /\ Y => match h with conj x y => conj y x end.
   Check fun h: (X /\ Y) /\ Z => match h with
                              conj (conj x y) z => conj x (conj y z)
                            end.
-  Check @or_introl X Y.
-  Check @or_intror X Y.
+
+  (** Note implicit argument overloading of or_introl and or_intror *)
+  
   Check fun h: X \/ Y => match h with
                        or_introl x => or_intror Y x
                      | or_intror y => or_introl X y
@@ -62,10 +72,20 @@ Section Demo.
   Check fun h: False => match h return X with end.  (* exfalso quodlibet *)
 
   Check fun x (f: ~X) => f x.
+  Check fun x (f: ~X) => elim_False Y (f x).
   Check fun x (f: ~X) => match f x return Y with end.
   Check fun (f: X -> ~X) g => let x := g (fun x => f x x) in f x x.
 
   (** Proof construction with tactics **)
+
+  Goal ~ ~X -> (X -> ~X) -> False.
+  Proof.
+    refine (fun f g => _).
+    refine (f _).
+    refine (fun x => _).
+    exact (g x x).
+    Show Proof.
+  Qed.
 
   Goal ~ ~X -> (X -> ~X) -> False.
   Proof.
@@ -76,13 +96,15 @@ Section Demo.
     Show Proof.
   Qed.
 
-  Goal ~ ~X -> (X -> ~X) -> False.
+  Goal ~(X <-> ~X).
   Proof.
-    refine (fun f g => f _).
-    refine (fun x => g x x).
+    refine (fun a => match a with conj f g => _ end).
+    refine (let x := _ in f x x).
+    refine (g (fun x => _)).
+    exact (f x x).
     Show Proof.
   Qed.
-
+  
   Fact Russell :
     ~(X <-> ~X).
   Proof.
@@ -93,36 +115,62 @@ Section Demo.
     Show Proof.
   Qed.
 
-  Goal ~(X <-> ~X).
-  Proof.
-    refine (fun h => match h with conj f g => _ end).
-    refine (let x := g (fun x => f x x) in _).
-    exact (f x x).
-    Show Proof.
-  Qed.
-
   Goal X /\ (Y \/ Z) <-> (X /\ Y) \/ (X /\ Z).
   Proof.
-    apply conj.
-    - intros [x [y|z]].
-      + exact (or_introl (conj x y)).
-      + exact (or_intror (conj x z)).
-    - intros [[x y]|[x z]].
+    refine (conj _ _).
+    - refine (fun a => match a with
+                    | conj x b => match b with
+                                 | or_introl y => _
+                                 | or_intror z => _
+                                 end
+                    end).
+      + exact (or_introl (conj x y)). 
+      + exact (or_intror (conj x z)). 
+    - refine (fun a => match a with
+                    | or_introl b => match b with conj x y => _ end
+                    | or_intror b => match b with conj x z => _ end
+                    end).
       + exact (conj x (or_introl y)).
       + exact (conj x (or_intror z)).
     Show Proof.
   Qed.
-
+  
   Goal X /\ (Y \/ Z) <-> (X /\ Y) \/ (X /\ Z).
   Proof.
     split.
     - intros [x [y|z]].
-      + left. auto.
-      + right. auto.
+      + left. split.
+        * exact x.
+        * exact y.
+      + right. split.
+        * exact x.
+        * exact z.
+    - intros [[x y]|[x z]].
+      + split.
+        * exact x.
+        * left. exact y.
+      + split.
+        * exact x.
+        * right. exact z.
+    Show Proof.
+  Qed.
+  
+  Goal X /\ (Y \/ Z) <-> (X /\ Y) \/ (X /\ Z).
+  Proof.
+    split.
+    - intros [x [y|z]].
+      + auto.
+      + auto.
     - intros [[x y]|[x z]].
       + auto.
       + auto.
     Show Proof.
+  Qed.
+  
+  Goal X /\ (Y \/ Z) <-> (X /\ Y) \/ (X /\ Z).
+  Proof.
+    tauto.
+    Show Proof.  (* Uses match functions *)
   Qed.
 
   Goal ~ ~(X -> Y) <-> (~ ~X -> ~ ~Y).
@@ -158,13 +206,6 @@ Section Demo.
     - intros x. exact (f' (f x)).
     - intros z. exact (g (g' z)).
   Show Proof.
-  Qed.
-
-  (**  Automation tactic tauto **)
-
-  Goal ~ ~(X -> Y) <-> (~ ~X -> ~ ~Y).
-  Proof.
-    tauto.
   Qed.
 
 End Demo.
@@ -223,14 +264,13 @@ Proof.
   Show Proof.
 Qed.
 
-(** Commands used:
-    Locate, About, Show Proof,
-    Section, Variables, End,     
-    Print, Check, Goal, Proof, Qed
+(** New Commands used:
+    About, Show Proof,
+    Section, Variables, End
  *)
 (** Tactics used:
+    refine, 
     intros, apply, exact, exfalso, assert, 
     split, left, right,
-    refine,
     auto, tauto   
  *)
