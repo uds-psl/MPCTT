353ca2590f1abf5a6e4259b32d05fed78989836a
smolka <smolka@ps.uni-saarland.de>
2021-05-12 10:49:35 +0200

    blue

--- coq/infty.v
+++ coq/infty.v
@@ -1,20 +1,105 @@
-From Coq Require Import Lia.
+Unset Elimination Schemes.
 
 Definition iffT (X Y: Type) : Type := (X -> Y) * (Y -> X).
 Notation "X <=> Y" := (iffT X Y) (at level 95, no associativity).
 
+Module Definitions.
+  Inductive sum (X Y: Type) : Type :=
+  | L: X -> sum X Y
+  | R: Y -> sum X Y.
+  Arguments L {X Y}.
+  Arguments R {X Y}.
+  
+  Definition match_sum {X Y Z: Type} 
+    : sum X Y -> (X -> Z) -> (Y -> Z) -> Z
+    := fun a e1 e2 => match a with L x => e1 x | R y => e2 y end.
+ 
+  Inductive sig {X: Type} (p: X -> Type) : Type :=
+  | E : forall x: X, p x -> sig p.
+  Arguments E {X p}.
+ 
+  Definition match_sig {X: Type} {p: X -> Type} {Z: Type}
+    : sig p -> (forall x, p x -> Z) -> Z
+    := fun a e => match a with E x c => e x c end.
+
+  Definition pi1 {X: Type} {p: X -> Type}
+    : sig p -> X
+    := fun a => match a with E x _ => x end.
+
+  Definition pi2 {X: Type} {p: X -> Type}
+    : forall a: sig p,  p (pi1 a)
+    := fun a => match a with E x c => c end.
+
+  Definition skolem X Y (p: X -> Y -> Type) :
+    sig (fun f => forall x, p x (f x)) <=> forall x, sig (p x).
+  Proof.
+    split.
+    - intros a.
+      apply (match_sig a). intros f H x.
+      exists (f x). apply H.
+    - intros H. exists (fun x => pi1 (H x)). intros x.
+      exact (pi2 (H x)).
+  Defined.
+    
+  Definition skolem' X Y (p: X -> Y -> Type) :
+    sig (fun f => forall x, p x (f x)) <=> forall x, sig (p x).
+  Proof.
+    split.
+    - intros [f H] x. exists (f x). apply H.
+    - intros H. exists (fun x => pi1 (H x)). intros x.
+      destruct (H x) as [y H1]. exact H1.
+  Defined.
+    
+  Definition elim_sig {X: Type} {p: X -> Type} (q: sig p -> Type)
+    : (forall x c, q (E x c)) -> forall a, q a
+    := fun e a => match a with E x c => e x c end.
+
+  Goal forall X (p: X -> Type),
+      @pi1 X p = elim_sig (fun _ => X) (fun x c => x).
+  Proof.
+    cbv. reflexivity.
+  Qed.
+
+  Goal forall X (p: X -> Type),
+      @pi2 X p = elim_sig (fun a => p (pi1 a)) (fun x c => c).
+  Proof.
+    reflexivity.
+  Qed.
+
+  Fact eta X (p: X -> Type) :
+    forall a: sig p, E (pi1 a) (pi2 a) = a.
+  Proof.
+    (* cannot be shown with match_sig *)
+    refine (elim_sig _ _).
+    cbn. reflexivity.
+  Qed.
+
+  
+  Fact pi1_injective X (p: X -> Type) x (c: p x) x' (c': p x') :
+    E x c = E x' c' -> x = x'.
+  Proof.
+  intros H.
+  change x with (pi1 (E x c)).
+  rewrite H. reflexivity.
+  Qed.
+
+  Fail
+    Fact pi2_injective X (p: X -> Type) x (c: p x) x' (c': p x') :
+    E x c = E x' c' -> c = c'.
+  
+  Fact pi2_injective X (p: X -> Type) x (c c': p x) :
+    E x c = E x c' -> c = c'.
+  Proof.
+    intros H.
+    change c with (pi2 (E x c)).
+    Fail pattern (E x c).
+    Fail rewrite H. 
+  Abort.
+
+End Definitions.
+
+Locate "+".
 Print sum.
-Print sigT.
-
-Definition match_sum
-  : forall X Y Z: Type, X + Y -> (X -> Z) -> (Y -> Z) -> Z
-  := fun X Y Z a f g => match a with inl x => f x | inr y => g y end.
-Arguments match_sum {X Y Z}.
-
-Definition match_sigT
-  : forall (X: Type) (p: X -> Type) (Z: Type), sigT p -> (forall x, p x -> Z) -> Z
-  := fun X p Z a f => match a with existT _ x c => f x c end.
-Arguments match_sigT {X p Z}.
 
 Definition nat_eqdec :
   forall x y: nat, (x = y) + (x <> y).
@@ -23,95 +108,42 @@ Proof.
   - left. reflexivity.
   - right. intros [=].
   - right. intros [=].
-  - apply (match_sum (IH y)); intros H.
-    (* destruct (IH y) as [H|H]. *)
+  - destruct (IH y) as [H|H].
     + left. f_equal. exact H.
-    + right. intros [= H1]. apply H, H1.
+    + right. intros [= <-]. easy.
 Defined.
 
+Print sigT.
+Notation sig := sigT.
+Notation Sig := existT.
+Notation pi1 := projT1.
+Notation pi2 := projT2.
+Notation "'Sigma' x .. y , p" :=
+  (sigT (fun x => .. (sigT (fun y => p)) ..))
+    (at level 200, x binder, right associativity,
+     format "'[' 'Sigma'  '/  ' x  ..  y ,  '/  ' p ']'")
+  : type_scope.
+
 Definition distance :
-  forall x y: nat, { z & sum (x + z = y) (y + z = x) }.
+  forall x y: nat, Sigma z, sum (x + z = y) (y + z = x).
 Proof.
-  induction x as [|x IH]. 2:destruct y.
-  - intros y. exists y. left. reflexivity.
-  - exists (S x). right. reflexivity.
-  - apply (match_sigT (IH y)). intros z a.
-    apply (match_sum a); intros H.
-    (* apply (IH y) as [z [H|H]]. *)
-    + exists z. left. cbn. f_equal. exact H.
-    + exists z. right. cbn. f_equal. exact H.
+  induction x as [|x IH]; cbn. 
+  - intros y. exists y. auto.
+  - destruct y; cbn.
+    + exists (S x). auto.
+    + specialize (IH y) as [z IH].
+      exists z. intuition.
 Defined.
 
 Fact sumAsSigma X Y:
-  X + Y <=> {b: bool & if b then X else Y}.
+  X + Y <=> Sigma b: bool, if b then X else Y.
 Proof.
   split.
   - intros [x|y].
       + exists true. exact x.
       + exists false. exact y.
-  - intros a. apply (match_sigT a). intros [|]; auto.
-Qed.
-
-(*** Projections and Skolem equivalence *)
-
-Definition pi1
-  : forall (X: Type) (p: X -> Type), sigT p -> X
-  := fun X p a => match a with existT _ x _ => x end.
-Arguments pi1 {X p}.
-Definition pi2
-  : forall (X: Type) (p: X -> Type) (a: sigT p),  p (pi1 a)
-  := fun X p a => match a with existT _ x c => c end.
-Arguments pi2 {X p}.
-
-Compute pi1 (distance 3 7).
-
-Fact pi1_eq X (p: X -> Type) x c :
-  pi1 (existT p x c) = x.
-Proof.
-  reflexivity.
-Qed.
-
-Fact pi2_eq X (p: X -> Type) x c :
-  pi2 (existT p x c) = c.
-Proof.
-  reflexivity.
-Qed.
-
-Fact Skolem X Y (p: X -> Y -> Type) :
-  {f & forall x, p x (f x)} <=> forall x, {y & p x y}.
-Proof.
-  split.
-  - intros a x. exists (pi1 a x). exact (pi2 a x).
-    (* intros [f H] x. exists (f x). exact (H x). *)
-  - intros F. exists (fun x => pi1 (F x)). intros x. exact (pi2 (F x)).
-Qed.
-
-Goal forall (X: Type) (p: X -> Type) (Z: Type),
-    sigT p -> (forall x, p x -> Z) -> Z.
-Proof.
-  intros X p Z a f. exact (f (pi1 a) (pi2 a)).
-Qed.
-
-Fact pi1_injective X (p: X -> Type) x c x' c' :
-  existT p x c = existT p x' c' -> x = x'.
-Proof.
-  intros H.
-  change x with (pi1 (existT p x c)).
-  rewrite H. reflexivity.
-Qed.
-
-Fail
-  Fact pi2_injective X (p: X -> Type) x c x' c' :
-  existT p x c = existT p x' c' -> c = c'.
-
-Fact pi2_injective X (p: X -> Type) x c c' :
-  existT p x c = existT p x c' -> c = c'.
-Proof.
-  intros H.
-  change c with (pi2 (existT p x c)).
-  Fail pattern (existT p x c).
-  Fail rewrite H. 
-Abort.
+  - intros [[|] z]; auto.
+Defined.
 
 Fact SkolemE X Y (p: X -> Y -> Prop) :
   (exists f, forall x, p x (f x)) <=> forall x, exists y, p x y.
@@ -121,46 +153,24 @@ Proof.
   - intros F.
 Abort.
 
-(*** Full eliminator *)
-
-Definition elim_sigT 
-  : forall (X: Type) (p: X -> Type) (q: sigT p -> Type),
-    (forall x c, q (existT _ x c)) -> forall a, q a
-  := fun X p q f a => match a with existT _ x c => f x c end.
-Arguments elim_sigT {X p q}.
-
-Fact sigT_eta X (p: X -> Type) :
-  forall a, a = existT p (pi1 a) (pi2 a).
+Fact sig_eta X (p: X -> Type) :
+  forall a, a = Sig p (pi1 a) (pi2 a).
 Proof.
-  apply elim_sigT. cbn. reflexivity.
+  intros [x H]. cbn. reflexivity.
 Qed.
 
-Fact pi1_elim X (p: X -> Type) :
-  pi1 (p:=p) = elim_sigT (q:= fun _ => X) (fun x c => x).
-Proof.
-  reflexivity.
-Qed.
-
-Fact pi2_elim X (p: X -> Type) (a: sigT p) :
-  pi2 (p:=p) = elim_sigT (q:= fun a => p (pi1 a)) (fun x c => c).
-Proof.
-  reflexivity.
-Qed.
+From Coq Require Import Lia.
 
 Goal forall x y, x - y = if pi2 (distance x y) then 0 else pi1 (distance x y).
 Proof.
   intros x y.
-  destruct (distance x y) as [z [H|H]]; cbn.
-  - subst y. lia.
-  - subst x. lia.
+  destruct (distance x y) as [z [<-|<-]]; cbn; lia.
 Qed.
 
 Goal forall x y, (x - y) + (y - x) = pi1 (distance x y).
 Proof.
   intros x y.
-  destruct (distance x y) as [z [H|H]]; cbn.
-  - subst y. lia.
-  - subst x. lia.
+  destruct (distance x y) as [z [<-|<-]]; cbn; lia.
 Qed.
 
 Module ProductAsSigma.
@@ -168,8 +178,8 @@ Section ProductAsSigma.
   Variables X Y: Type.
   Definition p (_:X) := Y.
   Definition f
-    : X * Y -> sigT p
-    := fun z => existT  p (fst z) (snd z).
+    : X * Y -> sig p
+    := fun z => Sig p (fst z) (snd z).
   Definition g
     : sigT p -> X * Y
     := fun a => (pi1 a, pi2 a).
@@ -177,14 +187,13 @@ Section ProductAsSigma.
   Fact gf_eq z :
     g (f z) = z.
   Proof.
-    destruct z as [x y]. cbv. reflexivity.
+    destruct z as [x y]. reflexivity.
   Qed.
 
   Fact fg_eq a :
     f (g a) = a.
   Proof.
-    revert a. apply elim_sigT. cbv. reflexivity.
-    (* destruct a as [x y]. reflexivity. *)
+    destruct a as [x y]. reflexivity.
   Qed.
 End ProductAsSigma.
 End ProductAsSigma.
@@ -194,33 +203,37 @@ Section SumAsSigma.
   Variables X Y: Type.
   Definition p (b: bool) := if b then X else Y.
   Definition f
-    : X + Y -> sigT p
-    := fun t => match_sum t (existT p true) (existT p false).
-
+    : X + Y -> sig p
+    := fun a => match a with
+             | inl x => Sig p true x
+             | inr y => Sig p false y
+             end.
+  
   Definition g
     : sigT p -> X + Y
     := fun a => match a with
-               existT _ true x => inl x
-             | existT _ false y => inr y
+             | Sig _ true x => inl x
+             | Sig _ false y => inr y
              end.
 
-  Fact gf_eq t :
-    g (f t) = t.
+  Fact gf_eq a :
+    g (f a) = a.
   Proof.
-    destruct t as [x|y]; cbn. all:reflexivity.
+    destruct a as [x|y]; reflexivity.
   Qed.
 
   Fact fg_eq a :
     f (g a) = a.
   Proof.
-    destruct a as [[|] z]; cbn. all:reflexivity.
+    destruct a as [[|] z]; reflexivity.
   Qed.
 End SumAsSigma.
 End SumAsSigma.
 
 (*** Truncation *)
 
-Inductive trunc (X: Type) : Prop := T (x:X).
+Inductive trunc (X: Type) : Prop :=
+| T: X -> trunc X.
 
 Goal forall P Q, P \/ Q <-> trunc (P + Q).
 Proof.
@@ -255,7 +268,7 @@ Goal forall P Q, ~ ~(P \/ Q) <-> ~(P + Q -> False).
 Proof.
   split.
   - intros H. contradict H. intros [a|a]; eauto.
-  - intros H. contradict H.intros [a|a]; eauto.
+  - intros H. contradict H. intros [a|a]; eauto.
 Qed.
 
 Check (True * True) <-> (True /\ True).
