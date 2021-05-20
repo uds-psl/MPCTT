9eece50f15a1eab84a7a1456f0bdcceec6510209
smolka <smolka@ps.uni-saarland.de>
2021-05-20 14:38:16 +0200

    green

--- coq/ext.v
+++ coq/ext.v
@@ -1,18 +1,34 @@
+Definition iffT (X Y: Type) : Type := (X -> Y) * (Y -> X).
+Notation "X <=> Y" := (iffT X Y) (at level 95, no associativity).
+Definition inv {X Y: Type} (g: Y -> X) (f: X -> Y) := forall x, g (f x) = x.
+Inductive bijection (X Y: Type) : Type :=
+| Bijection: forall (f: X -> Y) (g: Y -> X), inv g f -> inv f g -> bijection X Y.
+Arguments Bijection {X Y}.
+Definition dec (X: Type) := sum X (X -> False).
+Definition eqdec X := forall x y: X, dec (x = y).
+Fact inv_injective X Y f g x x' :
+  @inv X Y g f -> f x = f x' -> x = x'.
+Proof.
+  intros H H1. rewrite <-(H x), <-(H x').
+  f_equal. exact H1.
+Qed.
+
 Definition FE : Prop :=
   forall X (p: X -> Type) (f g: forall x, p x), (forall x, f x = g x) -> f = g.
 Definition PE : Prop :=
   forall P Q: Prop, P <-> Q -> P = Q.
-Definition unique (P: Prop) : Prop :=
-  forall a b: P, a = b.
+Definition unique (X: Type) : Prop :=
+  forall a b: X, a = b.
 Definition PI : Prop :=
   forall P: Prop, unique P.
 
-Fact unique_False :
+Fact unique_bot :
   unique False.
 Proof.
   intros [].
 Qed.
-Fact unique_True :
+
+Fact unique_top :
   unique True.
 Proof.
   intros [] []. reflexivity.
@@ -25,8 +41,74 @@ Proof.
   enough (unique P) as H1.
   { apply H1. }
   enough (P = True) as H1.
-  { rewrite H1. apply unique_True. }
+  { rewrite H1. apply unique_top. }
   apply H. tauto.
+Qed.
+
+Goal
+  FE -> bijection (True -> True) True.
+Proof.
+  intros H.
+  exists (fun _ => I) (fun _ x => x).
+  - intros f. apply H. intros x. apply unique_top.
+  - intros x. apply unique_top.
+Qed.
+
+Goal
+  FE -> bijection (True -> bool) bool.
+Proof.
+  intros H.
+  exists (fun f => f I) (fun b x => b).
+  - intros f. apply H. intros []. reflexivity.
+  - intros b. reflexivity.
+Qed.
+
+Definition encode (f: bool -> bool) : bool * bool :=
+  (f true, f false).
+
+Definition decode (a: bool * bool) : bool -> bool :=
+  match a with
+  | (true, true) => fun _ => true
+  | (true, false) => fun b => b
+  | (false, true) => negb
+  | (false, false) => fun _ => false
+  end.
+
+Goal
+  FE -> bijection (bool -> bool) (bool * bool).
+Proof.
+  intros H.
+  exists encode decode; hnf.
+  - intros f. apply H. intros x. unfold encode. 
+    destruct x, (f true), (f false); reflexivity.
+  - intros [x y]. unfold decode. destruct x, y; reflexivity.
+Qed.
+
+Goal forall X,  unique X -> eqdec X.
+Proof.
+  intros X H x y. left. apply H.
+Qed.
+
+Goal forall X,  bijection X True -> unique X.
+Proof.
+  intros X [f g H1 H2] x y.
+  rewrite <-(H1 x), <-(H1 y). f_equal.
+  apply unique_top.
+Qed.
+
+Goal forall X,  X -> unique X -> bijection X True.
+Proof.
+  intros X x.
+  exists (fun _ => I) (fun _ => x).
+  - intros y. apply H.
+  - intros a. apply unique_top.
+Qed.
+
+Fact unique_Coquand (A: Prop) (E: Prop -> A) (D: A -> Prop) :
+  unique A -> (forall P: Prop, D (E P) <-> P) -> False.
+Proof.
+  intros HA H.
+  apply H. rewrite (HA (E False) (E True)). apply H, I.
 Qed.
 
 Section Elim_restriction_or.
@@ -54,10 +136,3 @@ Section Elim_restriction_ex.
     revert H1. rewrite !feq. discriminate.
   Qed.
 End Elim_restriction_ex.
-
-Fact unique_Coquand (A: Prop) (E: Prop -> A) (D: A -> Prop) :
-  unique A -> (forall P, D (E P) <-> P) -> False.
-Proof.
-  intros HA H.
-  apply H. rewrite (HA (E False) (E True)). apply H, I.
-Qed.
