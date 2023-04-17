8ae6c94cee048de7080376b9c19621b70476ff75
smolka <smolka@ps.uni-saarland.de>
2023-04-17 14:09:56 +0200

    blah

--- coq/teaser-gs.v
+++ coq/teaser-gs.v
@@ -1,3 +1,6 @@
+(** Step through the teaser file and relate what you see to what is discussed in the first chapter “Getting Started” of MPCTT. You want to understand the inductive proofs first. Next try to understand how the inductive functions “min” and “iter” are defined in Coq. Note that “Fixpoint” is Coq’s keyword for defining recursive functions. *)
+
+
 Fact addO x :
   x + 0 = x.
 Proof.
@@ -49,6 +52,8 @@ Proof.
     + cbn. rewrite IH. reflexivity.
 Qed.
 
+(** Iteration *)
+
 Fixpoint iter (X: Type) (f: X -> X) (n:nat) (x:X) : X :=
   match n with
   | 0 => x
@@ -77,5 +82,31 @@ Proof.
   - cbn.  rewrite <-IH. reflexivity.
 Qed.
 
+(** Fibonacci function with iter and unfolding function *)
 
+Definition Fib f n :=
+  match n with
+  | 0 => 0
+  | 1 => 1
+  | S(S n) => f n + f (S n)
+  end.
 
+Check Fib.
+
+Definition step a := (snd a, fst a + snd a).
+
+Definition fib n := fst (iter step n (0,1)).
+
+Compute fib 10.
+
+Fact fib_correct n :
+  fib n = Fib fib n.
+Proof.
+  destruct n as [|n].
+  - reflexivity.
+  - destruct n as [|n].
+    + reflexivity.
+    + reflexivity.
+Qed.
+
+(* Remark: If you put a ";cbn" after the two destructs, the proof still goes through but you see things you don't want to see.  Getting simplification right in Coq is tricky. *)
