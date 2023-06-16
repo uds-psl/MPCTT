303813dcdb38bfe0c2d9be58475a5718d68f164f
smolka <smolka@ps.uni-saarland.de>
2023-06-16 11:42:33 +0200

    2023

---
+++ coq/teaser-list.v
@@ -0,0 +1,44 @@
+From Coq Require Import Arith Lia.
+Unset Elimination Schemes.
+Definition dec (X: Type) := sum X (X -> False).
+Definition eqdec X := forall x y: X, dec (x = y).
+Notation decidable p := (forall x, dec (p x)).
+Notation sig := sigT.
+Notation Sig := existT.
+Notation "'Sigma' x .. y , p" :=
+  (sigT (fun x => .. (sigT (fun y => p)) ..))
+    (at level 200, x binder, right associativity,
+     format "'[' 'Sigma'  '/  ' x  ..  y ,  '/  ' p ']'")
+  : type_scope.
+From Coq Require Import List.
+Import ListNotations.
+Notation "x 'el' A" := (In x A) (at level 70).
+
+Section List1.
+  Variable X : Type.
+  Implicit Types A B : list X.
+
+  Goal forall x A B,
+      x el A ++ B <-> x el A \/ x el B.
+  Proof.
+    intros x A B.
+    induction A as [|a A IH]; cbn.
+    - intuition.
+    - intuition.
+  Qed.
+  
+  Goal eqdec X -> eqdec (list X).
+  Proof.
+    intros d A.
+    induction A as [|a A IH]; cbn.
+    - intros [|x B].
+      + left. easy.
+      + right. easy.
+    - intros [|x B].
+      + right. easy.
+      + destruct (d a x) as [H|H].
+        * destruct (IH B) as [H1|H2].
+          -- left. congruence.
+          -- right. congruence.
+        * right. congruence.
+  Qed.
