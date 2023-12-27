ddc15eedc2e3e6fd8b707d7c3d3d69367c4f34cd
smolka <smolka@ps.uni-saarland.de>
2023-12-27 19:43:18 +0100

    green

--- coq/ewo.v
+++ coq/ewo.v
@@ -1,4 +1,4 @@
-(*** Higher-Order Structural Recursion and EWOs *)
+(*** EWOs *)
 From Coq Require Import Lia.
 Definition iffT (X Y: Type) : Type := (X -> Y) * (Y -> X).
 Notation "X <=> Y" := (iffT X Y) (at level 95, no associativity).
