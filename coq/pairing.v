084e924f669262a56a3ad9111238ac2d40c5e4ac
smolka <smolka@ps.uni-saarland.de>
2024-05-16 13:06:05 +0200

    green

--- coq/pairing.v
+++ coq/pairing.v
@@ -25,6 +25,14 @@ Fixpoint sum n : nat :=
 Definition encode '(x, y) : nat :=
   sum (x + y) + y.
 
+Fact encode_zero x y :
+  encode (x,y) = 0 -> x = 0 /\ y = 0.
+Proof.
+  destruct x.
+  - destruct y. easy. easy.
+  - easy.
+Qed.
+
 Fact encode_next a :
   encode (next a) = S (encode a).
 Proof.
@@ -33,14 +41,30 @@ Proof.
   - replace (x + S y) with (S (x +y)) by lia. cbn. lia.
 Qed.
 
-Opaque encode. (* Disable simplification of encode *)
+(* Disable simplification of encode
+   without affecting reflexivity and easy. *)
+Opaque encode. 
+
+Fact encode_eq_zero x n :
+  encode (S x, 0) = S n -> encode (0, x) = n.
+Proof.
+  change (S x, 0) with (next (0,x)).
+  rewrite encode_next. congruence.
+Qed.
+
+Fact encode_eq_S x y n :
+  encode (x, S y) = S n -> encode (S x, y) = n.
+Proof.
+  change (x, S y) with (next (S x, y)). 
+  rewrite encode_next. congruence.
+Qed.
 
 Fact encode_decode n :
   encode (decode n) = n.
 Proof.
   induction n as [|n IH]; cbn.
   - reflexivity.
-  - rewrite encode_next, IH. reflexivity.
+  - rewrite encode_next. congruence.
 Qed.
 
 Fact decode_encode a :
@@ -49,14 +73,11 @@ Proof.
   revert a.
   enough (forall n a, encode a = n -> decode n = a) by eauto.
   induction n as [|n IH]; intros [x y]; cbn.
-  - destruct x, y; easy.
+  - intros [-> ->] %encode_zero. reflexivity.
   - destruct y.
     + destruct x. easy.
-      change (S x, 0) with (next (0,x)).
-      rewrite encode_next.
-      specialize (IH (0,x)). 
-      intros [= <-]. f_equal. auto.
-    + change (x, S y) with (next (S x, y)). 
-      rewrite encode_next.
-      intros [= <-]. f_equal. auto. 
+      intros <-%encode_eq_zero.
+      rewrite (IH (0,x)); reflexivity.
+    + intros <-%encode_eq_S.
+      rewrite (IH (S x, y)); reflexivity.
 Qed.
