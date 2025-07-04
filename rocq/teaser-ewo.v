e29c6732794a4e6f7724b7c83006f1bcf1ac33ad
smolka <smolka@ps.uni-saarland.de>
2025-07-04 09:36:29 +0200

    green

--- rocq/teaser-ewo.v
+++ rocq/teaser-ewo.v
@@ -1,50 +1,42 @@
 From Stdlib Require Import Lia.
 Definition dec (X: Type) : Type := X + (X -> False).
 Definition eqdec X := forall x y: X, dec (x = y).
-Notation decidable p := (forall x, dec (p x)).
+Notation decider p := (forall x, dec (p x)).
 Notation sig := sigT.
 Notation Sig := existT.
+Inductive void : Type := .
+Inductive unit : Type := U.
+Definition inv {X Y: Type} (g: Y -> X) (f: X -> Y) :=
+  forall x, g (f x) = x.
+Inductive injection (X Y: Type) : Type :=
+| Injection {f: X -> Y} {g: Y -> X} (H: inv g f).
 
-Implicit Types n k: nat.
+Definition EWO (X: Type) :=
+  forall p: X -> Prop, decider p -> ex p -> sig p.
 
-Section EWO.
+Fact injection_ewo X Y :
+  injection X Y -> EWO Y -> EWO X.
+Proof.
+  intros [f g H] E p p_dec H1.
+  destruct (E (fun y => p (g y))) as [a Ha].
+  - intros x. destruct (p_dec (g x)); unfold dec; auto.
+  - destruct H1 as [x Hx]. exists (f x). congruence.
+  - eauto.
+Qed.
+  
+Section EWO_Nat.
   Variable p: nat -> Prop.
-  Variable p_dec: decidable p.
+  Variable p_dec: decider p.
 
   Inductive T (n: nat) : Prop := C (phi: ~p n -> T (S n)).
 
-  Definition W'
-    : forall n, T n -> sig p
-    := fix f n a := let (phi) := a in
-                    match p_dec n with
-                    | inl h => (Sig p n h)
-                    | inr h => f (S n) (phi h)
-                    end.
-
-  (* Eliminator generalizing W' *)
+  Fixpoint W n (a: T n) : sig p :=
+    match a with C _ phi => match p_dec n with
+                       | inl h => (Sig p n h)
+                       | inr h => W (S n) (phi h)
+                       end
+    end.
   
-  Definition elim_T (q: nat -> Type)
-    : (forall n, (~p n -> q (S n)) -> q n) ->
-      forall n, T n -> q n
-    := fun e => fix f n a := let (phi) := a in e n (fun h => f (S n) (phi h)).
-
-
-  Fact W'_elim_T_agree :
-    W' = elim_T (fun _ => sig p)
-           (fun n f => match p_dec n with
-                    | inl h => (Sig p n h)
-                    | inr h => f h
-                    end).
-  Proof.
-    reflexivity.
-  Qed.
-  
-  Fact elim_T_unfold q e n phi :
-    elim_T q e n (C n phi) = e n (fun h => elim_T q e (S n) (phi h)).
-  Proof.
-    reflexivity.
-  Qed.
-
   Lemma T_lower n :
     T n -> T 0.
   Proof.
@@ -52,38 +44,15 @@ Section EWO.
     intros H. apply IH. constructor. auto.
   Qed.
 
-  Definition W : ex p -> sig p.
-  Proof.
-    intros H. eapply W' with (n:=0).
-    destruct H as [n H].
-    enough (T n) as H1%T_lower by easy.
-    constructor. easy.
-  Qed.
-
-End EWO.
-
-Definition inv {X Y: Type} (g: Y -> X) (f: X -> Y) := forall x, g (f x) = x.
-
-Section Countable_EWO.
-  Variable X: Type.
-  Variable f: X -> nat.
-  Variable g: nat -> X.
-  Variable gf: inv g f.
-  Variable p: X -> Prop.
-  Variable p_dec: decidable p.
-
-  Definition cewo : ex p -> sig p.
+  Fact ewo_nat' : ex p -> sig p.
   Proof.
     intros H.
-    pose (q n := p (g n)).
-    assert (q_dec: decidable q).
-    { intros n. apply p_dec. }
-    assert (q_ex: ex q).
-    { destruct H as [x H]. exists (f x). hnf. congruence. }
-    enough (sig q) as [n H1].
-    { exists (g n). exact H1. }
-    apply W; assumption.
+    apply W with (n:=0).
+    destruct H as [n H].
+    apply (T_lower n).
+    constructor.
+    easy.
   Qed.
-End Countable_EWO.
-    
-      
+End EWO_Nat.
+
+Definition ewo_nat : EWO nat := ewo_nat'.
