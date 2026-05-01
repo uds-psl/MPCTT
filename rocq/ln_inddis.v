0ea5a65633bb1d9a8296d039c41d8ccd4c56035f
smolka <smolka@ps.uni-saarland.de>
2026-05-01 13:30:42 +0200

    green

--- rocq/ln_inddis.v
+++ rocq/ln_inddis.v
@@ -1,12 +1,12 @@
 From Stdlib Require Import Lia.
 
-Fixpoint ind_nat (p: nat -> Prop)
+Fixpoint induction_nat (p: nat -> Prop)
   (a: p 0) (f: (forall n, p n -> p (S n)))
   (n : nat) : p n.
 Proof.
   destruct n.
   - exact a.
-  - exact (f n (ind_nat p a f n)).
+  - exact (f n (induction_nat p a f n)).
 Qed.
 
 Fact discriminate_nat (p: nat -> Prop) :
@@ -15,12 +15,6 @@ Proof.
   intros a f [|n].
   - exact a.
   - exact (f n).
-Qed.
-
-Fact discriminate_pair (X Y: Type) (p: X * Y -> Prop) :
-  (forall x y, p (x,y)) -> forall a, p a.
-Proof.
-  intros f [x y]. exact (f x y).
 Qed.
 
 (** We prove [S n <> n] using the induction rule
@@ -40,7 +34,7 @@ Qed.
 
 Goal forall n, S n <> n.
 Proof.
-  refine (ind_nat _ _ _).
+  refine (induction_nat _ _ _).
   - apply nat_S_O.
   - intros n IH H. apply IH. apply nat_S_injective, H.
 Qed.
@@ -56,15 +50,6 @@ Proof.
   (* congruence knows the constructor laws *)
 Qed.
 
-(* Eta law for pairs *)
-
-Goal forall X Y, forall a: X * Y, a = (fst a, snd a).
-Proof.
-  intros X Y.
-  refine (discriminate_pair _ _ _ _).
-  cbn. reflexivity.
-Qed.
-
 (** Order on numbers *)
 
 Notation "x <= y" := (x - y = 0) : nat_scope.
@@ -78,7 +63,7 @@ Qed.
 
 Goal forall x, x <= x.
 Proof.
-  refine (ind_nat _ _ _); cbn; easy.
+  refine (induction_nat _ _ _); cbn; easy.
 Qed.
 
 Goal forall x y z, x <= y -> y <= z -> x <= z.
@@ -91,7 +76,7 @@ Qed.
 
 Goal forall x y z, x <= y -> y <= z -> x <= z.
 Proof.
-  refine (ind_nat _ _ _); cbn. easy.
+  refine (induction_nat _ _ _); cbn. easy.
   intros x IH.
   refine (discriminate_nat _ _ _); cbn. easy.  (* easy knows the first constructor law *)
   intros y.
@@ -119,6 +104,8 @@ Proof.
     + exact (IHx y).
 Qed.
 
+(** Equality decider for numbers *)
+
 Fact plus_and x y :
   x + y = 0 <-> x = 0 /\ y = 0.
 Proof.
@@ -135,4 +122,92 @@ Proof.
   - intros <-. split; apply le_refl.
   - intros [H1 H2]. apply le_antisym; assumption.
 Qed.
-  
+
+(** Pairs *)
+
+Fact discriminate_pair {X Y: Type} (p: X * Y -> Prop) :
+  (forall x y, p (x,y)) -> forall a, p a.
+Proof.
+  intros f [x y]. exact (f x y).
+Qed.
+
+Fact eta X Y :
+  forall a: X * Y, a = (fst a, snd a).
+Proof.
+   refine (discriminate_pair _ _).
+  cbn. reflexivity.
+Qed.
+
+Definition M_pair {X Y Z} (f: X -> Y -> Z) (a: X * Y) : Z
+  := match a with (x,y) => f x y end.
+
+Fact pair_injective {X Y x y} {x':X} {y':Y} :
+  (x,y) = (x',y') -> x = x' /\ y = y'.
+Proof.
+  intros e.
+  change (M_pair (fun x y => x = x' /\ y = y') (x,y)).
+  rewrite e. cbn. easy.
+Qed.
+
+Goal forall a, a = (0,0) \/ a <> (0,0).
+Proof.
+  refine (discriminate_pair _ _).
+  refine (discriminate_nat _ _ _).
+  - refine (discriminate_nat _ _ _).
+    + left. reflexivity.
+    + right. intros H.
+      apply pair_injective in H.
+      apply (nat_S_O n).
+      apply H.
+  - refine (discriminate_nat _ _ _).
+    + right. intros H.
+      apply pair_injective in H.
+      apply (nat_S_O 0).
+      apply H.
+    + right. intros H.
+      apply pair_injective in H.
+      eapply (nat_S_O (S n)).
+      apply H.
+Qed.
+
+Goal forall a, a = (0,0) \/ a <> (0,0).
+Proof.
+  intros [[|x] [|y]]; cbn.
+  - left. reflexivity.
+  - right. easy.
+  - right. easy.
+  - right. easy.
+Qed.
+
+Goal forall a, a = (0,0) \/ a <> (0,0).
+Proof.
+  intros [[|x] [|y]]; intuition easy.
+Qed.
+
+Fact nat_eq :
+  forall x y : nat, x = y \/ x <> y.
+Proof.
+  refine (induction_nat _ _ _).
+  - refine (discriminate_nat _ _ _).
+    + left; reflexivity.
+    + right. symmetry. apply nat_S_O.
+  - intros x IH.
+    refine (discriminate_nat _ _ _).
+    + right. apply nat_S_O.
+    + intros y. destruct (IH y) as [-> |H].
+      * left; reflexivity.
+      * right. contradict H. apply nat_S_injective, H.
+Qed.
+
+Goal forall a b: nat*nat, a = b \/ a <> b.
+Proof.
+  intros [x y] [x' y'].
+  destruct (nat_eq x x') as [-> |Hx].
+  - destruct (nat_eq y y') as [-> |Hy].
+    + left. reflexivity.
+    + right. contradict Hy.
+      apply pair_injective in Hy. apply Hy.
+  - right. contradict Hx.
+    apply pair_injective in Hx. apply Hx.
+Qed.
+
--- rocq/pairing.v
+++ rocq/pairing.v
@@ -5,29 +5,28 @@
    demonstrate several advanced tactic uses.  *)
 
 From Stdlib Require Import Lia.
-
 Implicit Types (n x y: nat) (a: nat * nat).
 
-Definition eta a : nat * nat :=
+Definition eta a :=
   match a with
   | (0,y) => (S y, 0)
   | (S x, y) => (x, S y)
   end.
 
-Fixpoint decode n : nat * nat :=
+Fixpoint decode n :=
   match n with
   | 0 => (0,0)
-  | S n' => eta (decode n')
+  | S n => eta (decode n)
   end.
 
-Fixpoint sum n : nat :=
+Fixpoint sigma n :=
   match n with
   | 0 => 0
-  | S n' => S n' + sum n'
+  | S n => sigma n + S n
   end.
 
-Definition encode '(x, y) : nat :=
-  sum (x + y) + y.
+Definition encode '(x, y) :=
+  sigma (x + y) + y.
 
 Fact encode_eta a :
   encode (eta a) = S (encode a).
@@ -65,13 +64,13 @@ Proof.
   revert a.
   enough (forall n a, encode a = n -> decode n = a) by eauto.
   induction n as [|n IH]; cbn; intros a.
-  - destruct a as [[|x] [|y]]; cbn; easy.
+  - destruct a as [[|x] [|y]]; cbn; (lia || easy).
   - destruct (eta_pi a) as [-> | ->]; cbn. easy.
     rewrite encode_eta. intros [= H1%IH]. congruence.
 Qed.
 
 Fact Gauss n :
-  2 * sum n = n * S n.
+  2 * sigma n = n * S n.
 Proof.
   induction n as [|n IH]; cbn; lia.
 Qed.
