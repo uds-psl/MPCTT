003dc85692cc7a43b30a28326ba3704e727dc0be
smolka <smolka@ps.uni-saarland.de>
2026-05-02 10:35:53 +0200

    green

--- rocq/pairing.v
+++ rocq/pairing.v
@@ -7,41 +7,41 @@
 From Stdlib Require Import Lia.
 Implicit Types (n x y: nat) (a: nat * nat).
 
-Definition eta a :=
+Definition sigma a :=
   match a with
   | (0,y) => (S y, 0)
   | (S x, y) => (x, S y)
   end.
 
-Fixpoint decode n :=
+Fixpoint D n :=
   match n with
   | 0 => (0,0)
-  | S n => eta (decode n)
+  | S n => sigma (D n)
   end.
 
-Fixpoint sigma n :=
+Fixpoint gamma n :=
   match n with
   | 0 => 0
-  | S n => sigma n + S n
+  | S n => gamma n + S n
   end.
 
-Definition encode '(x, y) :=
-  sigma (x + y) + y.
+Definition E '(x, y) :=
+  gamma (x + y) + y.
 
-Fact encode_eta a :
-  encode (eta a) = S (encode a).
+Fact E_sigma a :
+  E (sigma a) = S (E a).
 Proof.
   destruct a as [[|x] y]; cbn.
   - replace (y + 0) with y; lia.
   - replace (x + S y) with (S (x + y)); cbn; lia.
 Qed.
 
-Fact encode_decode n :
-  encode (decode n) = n.
+Fact E_D n :
+  E (D n) = n.
 Proof.
   induction n as [|n IH]; cbn.
   - reflexivity.
-  - rewrite encode_eta. congruence.
+  - rewrite E_sigma. congruence.
 Qed.
 
 Definition pi a :=
@@ -52,26 +52,26 @@ Definition pi a :=
   end.
 Arguments pi : simpl nomatch.
 
-Fact decode_encode a :
-  decode (encode a) = a.
+Fact D_E a :
+  D (E a) = a.
 Proof.
   revert a.
-  enough (forall n a, encode a = n -> decode n = a) by eauto.
+  enough (forall n a, E a = n -> D n = a) by eauto.
   induction n as [|n IH]; cbn; intros a.
   - destruct a as [[|x] [|y]]; cbn; auto; lia.
-  - assert (a = (0,0) \/ a = eta (pi  a)) as [-> | ->]; cbn.
+  - assert (a = (0,0) \/ a = sigma (pi  a)) as [-> | ->]; cbn.
     + destruct a as [[|x] [|y]]; cbn; auto.
     + easy.
-    + rewrite encode_eta. intros [= H1%IH]. congruence.
+    + rewrite E_sigma. intros [= H1%IH]. congruence.
 Qed.
 
 Fact Gauss n :
-  2 * sigma n = n * S n.
+  2 * gamma n = n * S n.
 Proof.
   induction n as [|n IH]; cbn; lia.
 Qed.
 
-Goal forall a, pi (eta a) = a.
+Goal forall a, pi (sigma a) = a.
 Proof.
   intros [[|[|x]] [|y]]; cbn; easy.
 Qed.
